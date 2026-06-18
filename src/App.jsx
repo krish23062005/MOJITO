22e74b83ebea5fc2c7ac74a8a073058aee697599
Krish Patel <krishpatel23062005@gmail.com>
2026-06-18 10:36:43 +0530

    implement the art section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from "./components/Hero"
 import { Agentation } from "agentation"
 import Cocktails from "./components/Cocktails"
 import About from "./components/About"
+import Art from "./components/Art"
 
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
@@ -18,6 +19,7 @@ const App = () =>{
         <Hero />
         <Cocktails />
         <About/>
+        <Art/>
         {import.meta.env.DEV && <Agentation />}
     </main>
   )
